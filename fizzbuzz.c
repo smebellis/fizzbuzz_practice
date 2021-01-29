8ece60b82689746f750f19fa5efd082f8892ae05
ryan.ellis33@gmail.com <ryan.ellis33@gmail.com>
2021-01-29 10:52:40 -0700

    add fizz

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -5,7 +5,11 @@ int main()
 {
     for (int i = 0; i <= 100; i++)
     {
-        printf("%d \n", i); 
+        if (i % 3 == 0)
+        {
+            printf("fizz\n");
+        }
+        printf("%d \n", i);
     }
 
     return 0;
